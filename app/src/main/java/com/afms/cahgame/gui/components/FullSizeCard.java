a445652d930d65c03aeb46f9fa81ce8af495614e
Pii <piapurr@freenet.de>
2019-04-16 15:15:40 +0200

    added swipe up animation/motion to fullSizeCard

--- app/src/main/java/com/afms/cahgame/gui/components/FullSizeCard.java
+++ app/src/main/java/com/afms/cahgame/gui/components/FullSizeCard.java
@@ -1,5 +1,9 @@
 package com.afms.cahgame.gui.components;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.Observer;
@@ -8,6 +12,8 @@ import android.graphics.Color;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,9 +32,14 @@ public class FullSizeCard extends ConstraintLayout {
     private Button fullSizeCardButton;
     private Button fullSizeCardButton2;
     private Card card;
+    private float downPos;
+    private float movedPos;
+    private float oldPos;
+    private float newPos;
 
     private Activity mainActivity;
 
+    @SuppressLint("ClickableViewAccessibility")
     public FullSizeCard(Context context, Card card) {
         super(context);
         this.card = card;
@@ -45,16 +56,53 @@ public class FullSizeCard extends ConstraintLayout {
         fullSizeGameName.setTextColor(card.getColour() == Colour.WHITE ? ContextCompat.getColor(context, R.color.cardTextColorWhite) : Color.WHITE);
         fullSizeCardButton = findViewById(R.id.fullSizeOptionButton);
         fullSizeCardButton.setText(context.getString(R.string.close));
-        fullSizeCardButton.setOnClickListener(v -> ((ViewManager) getParent()).removeView(this));
+        fullSizeCardButton.setOnClickListener(v -> {
+            ((ViewManager) getParent()).removeView(this);
+            fullSizeCardLayout.setY(oldPos);
+        });
         fullSizeCardButton2 = findViewById(R.id.fullSizeOptionButton2);
         ((ViewManager) fullSizeCardButton2.getParent()).removeView(fullSizeCardButton2);
+
+        oldPos = fullSizeCardLayout.getY();
 
         // Observer
         final Observer<String> fullSizeCardTextObserver = fullSizeCardText::setText;
         card.getText().observe((LifecycleOwner) mainActivity, fullSizeCardTextObserver);
+
+        fullSizeCardLayout.setOnTouchListener((v, event) -> {
+            switch(event.getAction()){
+                case MotionEvent.ACTION_DOWN:
+                    downPos = event.getY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    movedPos = event.getY();
+                    newPos = v.getY()+ movedPos-downPos;
+                    v.setY(newPos);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    if(v.getY() > -1100){
+                        v.setY(oldPos);
+                    }else{
+                        ObjectAnimator animation = ObjectAnimator.ofFloat(v, "translationY", -2800f);
+                        animation.setDuration(400);
+                        animation.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                ((ViewManager) v.getParent().getParent().getParent()).removeView(((View)v.getParent().getParent()));
+                                v.setY(oldPos);
+                            }
+                        });
+                        animation.start();
+                    }
+                    break;
+            }
+            return false;
+        });
     }
 
     public Card getCard() {
         return card;
     }
+
 }
