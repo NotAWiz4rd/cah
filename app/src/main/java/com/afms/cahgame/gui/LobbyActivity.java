239a3f217217bbd51293a92696e739242d23097f
Lucas Hensel <l.hensel@ostfalia.de>
2019-05-10 10:34:48 +0200

    refactoring

--- app/src/main/java/com/afms/cahgame/gui/LobbyActivity.java
+++ app/src/main/java/com/afms/cahgame/gui/LobbyActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.widget.Button;
 
 import com.afms.cahgame.R;
 import com.afms.cahgame.game.Card;
@@ -22,11 +22,9 @@ public class LobbyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lobby);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_create_lobby);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        Button fab = findViewById(R.id.btn_create_lobby_create_lobby);
         fab.setOnClickListener(view -> createLobby());
     }
 
