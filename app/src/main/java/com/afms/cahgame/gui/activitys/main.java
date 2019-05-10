d418254f010ad52a57bc5d436cafc416b6fc7a77
Lucas Hensel <l.hensel@ostfalia.de>
2019-05-10 10:49:26 +0200

    fixes

---
+++ app/src/main/java/com/afms/cahgame/gui/activitys/main.java
@@ -0,0 +1,15 @@
+package com.afms.cahgame.gui.activitys;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.afms.cahgame.R;
+
+public class main extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
