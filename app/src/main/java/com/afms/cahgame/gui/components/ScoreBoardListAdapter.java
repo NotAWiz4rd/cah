407d9d4ae5c8f28870812a889758e7642105a234
Pii <piapurr@freenet.de>
2019-05-27 17:08:53 +0200

    updated score board (not final) and removed some warnings

--- app/src/main/java/com/afms/cahgame/gui/components/ScoreBoardListAdapter.java
+++ app/src/main/java/com/afms/cahgame/gui/components/ScoreBoardListAdapter.java
@@ -10,27 +10,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.afms.cahgame.R;
+import com.afms.cahgame.game.Card;
+import com.afms.cahgame.game.Game;
 import com.afms.cahgame.game.Player;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static android.content.Context.MODE_PRIVATE;
 
 public class ScoreBoardListAdapter extends ArrayAdapter<Player> {
 
     private Player roundWinner;
-    private SharedPreferences settings;
+    private Game game;
 
-    public ScoreBoardListAdapter(@NonNull Context context, ArrayList<Player> players, Player roundWin) {
+    public ScoreBoardListAdapter(@NonNull Context context, ArrayList<Player> players, Player roundWin, Game game) {
         super(context, 0, players);
         roundWinner = roundWin;
+        this.game = game;
     }
 
-    public ScoreBoardListAdapter(@NonNull Context context, ArrayList<Player> players) {
+    public ScoreBoardListAdapter(@NonNull Context context, ArrayList<Player> players, Game game) {
         super(context, 0, players);
+        this.game = game;
     }
 
     @SuppressLint("DefaultLocale")
@@ -44,19 +50,36 @@ public class ScoreBoardListAdapter extends ArrayAdapter<Player> {
 
         TextView playerName = convertView.findViewById(R.id.score_board_player_name);
         TextView playerScore = convertView.findViewById(R.id.score_board_player_score);
+        TextView playedCard = convertView.findViewById(R.id.score_board_card_text);
+        ImageView scoreIcon = convertView.findViewById(R.id.score_board_score_icon);
+        ImageView icon = convertView.findViewById(R.id.score_board_player_icon);
 
-        settings = getContext().getSharedPreferences("Preferences", MODE_PRIVATE);
+        SharedPreferences settings = getContext().getSharedPreferences("Preferences", MODE_PRIVATE);
         if (player != null) {
             if (player.getName().equals(settings.getString("player", ""))) {
                 playerName.setTypeface(ResourcesCompat.getFont(getContext(), R.font.helveticaneuebold));
             } else {
                 playerName.setTypeface(ResourcesCompat.getFont(getContext(), R.font.helveticaneuelight));
             }
-
+            if(roundWinner != null){
+                List<Card> playedCardList = game.getPlayedCards();
+                for (Card card: playedCardList) {
+                    if(card.getOwner().getName().equals(player.getName())){
+                        playedCard.setText(card.getText());
+                    }
+                }
+            }
             if (roundWinner != null && roundWinner.getName().equals(player.getName())) {
-                playerName.setTextColor(getContext().getColor(R.color.pastel_green));
+                playerName.setTextColor(getContext().getColor(R.color.pastel_green_darker));
+                scoreIcon.setBackground(getContext().getDrawable(R.drawable.img_score));
             } else {
                 playerName.setTextColor(getContext().getColor(R.color.inputTextColorBlack));
+                scoreIcon.setBackground(null);
+                playedCard.setText(null);
+            }
+
+            if(game.getCardCzar().equals(player.getName())){
+                icon.setBackground(getContext().getDrawable(R.drawable.vector_eye_black_24dp));
             }
             playerName.setText(player.getName());
             playerScore.setText(String.valueOf(player.getScore()));
