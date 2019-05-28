9859201658bfddfe97fe2bc86aad6fb716259830
Pii <piapurr@freenet.de>
2019-05-28 11:59:54 +0200

    updated score board

--- app/src/main/java/com/afms/cahgame/gui/components/ScoreBoardListAdapter.java
+++ app/src/main/java/com/afms/cahgame/gui/components/ScoreBoardListAdapter.java
@@ -6,11 +6,13 @@ import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.res.ResourcesCompat;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.afms.cahgame.R;
@@ -50,18 +52,20 @@ public class ScoreBoardListAdapter extends ArrayAdapter<Player> {
 
         TextView playerName = convertView.findViewById(R.id.score_board_player_name);
         TextView playerScore = convertView.findViewById(R.id.score_board_player_score);
-        TextView playedCard = convertView.findViewById(R.id.score_board_card_text);
+        RelativeLayout cardTextLayout = convertView.findViewById(R.id.score_board_card_text_layout);
         ImageView scoreIcon = convertView.findViewById(R.id.score_board_score_icon);
         ImageView icon = convertView.findViewById(R.id.score_board_player_icon);
 
         SharedPreferences settings = getContext().getSharedPreferences("Preferences", MODE_PRIVATE);
         if (player != null) {
+            cardTextLayout.removeAllViews();
             if (player.getName().equals(settings.getString("player", ""))) {
                 playerName.setTypeface(ResourcesCompat.getFont(getContext(), R.font.helveticaneuebold));
             } else {
                 playerName.setTypeface(ResourcesCompat.getFont(getContext(), R.font.helveticaneuelight));
             }
             if(roundWinner != null){
+                TextView playedCard = createCardTexTView(cardTextLayout);
                 List<Card> playedCardList = game.getRoundEndPlayedCards();
                 for (Card card: playedCardList) {
                     if(card.getOwner().getName().equals(player.getName())){
@@ -75,25 +79,38 @@ public class ScoreBoardListAdapter extends ArrayAdapter<Player> {
                         playedCard.setText(getContext().getString(R.string.no_played_card));
                     }
                 }
-            }
-            if (roundWinner != null && roundWinner.getName().equals(player.getName())) {
-                playerName.setTextColor(getContext().getColor(R.color.pastel_green_darker));
-                scoreIcon.setBackground(getContext().getDrawable(R.drawable.img_score));
-            } else {
-                playerName.setTextColor(getContext().getColor(R.color.inputTextColorBlack));
-                scoreIcon.setBackground(null);
+                if (roundWinner.getName().equals(player.getName())) {
+                    playerName.setTextColor(getContext().getColor(R.color.pastel_green_darker));
+                    scoreIcon.setBackground(getContext().getDrawable(R.drawable.img_score));
+                } else {
+                    playerName.setTextColor(getContext().getColor(R.color.inputTextColorBlack));
+                    scoreIcon.setBackground(null);
+                }
+                cardTextLayout.addView(playedCard);
             }
 
             if(game.getCardCzar().equals(player.getName())){
                 icon.setBackground(getContext().getDrawable(R.drawable.vector_eye_black_24dp));
+            } else {
+                icon.setBackground(getContext().getDrawable(R.drawable.vector_person_24dp));
             }
             playerName.setText(player.getName());
             playerScore.setText(String.valueOf(player.getScore()));
         }
-
         convertView.setEnabled(false);
         convertView.setOnClickListener(null);
 
         return convertView;
+    }
+
+    private TextView createCardTexTView(View parent){
+        TextView cardText = new TextView(parent.getContext());
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(6, 0, 6, 0);
+        cardText.setLayoutParams(lp);
+        cardText.setBackground(getContext().getDrawable(R.drawable.bg_white_transparent));
+        cardText.setGravity(Gravity.CENTER_HORIZONTAL);
+        cardText.setTextColor(getContext().getColor(android.R.color.black));
+        return cardText;
     }
 }
